8703a98e7e0aee13da68fec1273394b687d56c51
NarekAstoyan <narek.astoyan@tumo.org>
2023-04-23 19:39:01 +0400

    server

--- script.js
+++ script.js
@@ -1,54 +1,17 @@
-
+var socket = io()
 let side = 30
 ///օբյեկտներ պահելու զանգվածներ
-var grassArr = []
-var grassEaterArr = []
-var predatorArr = []
-var flowerArr = []
-var rabbitArr = []
-var waterArr = []
-var fireArr = []
+
 
 
 function setup() {
-        frameRate(15)
-        createCanvas(matrix[0].length * side, matrix.length * side)
-        for (let y = 0; y < matrix.length; y++) {
-                for (let x = 0; x < matrix[y].length; x++) {
-                        if (matrix[y][x] == 1) {
-                                let grass = new Grass(x, y)
-
-                                grassArr.push(grass)
-
-
-                        } else if(matrix[y][x] == 2){
-                             let grEat = new  GrassEater(x,y)
-                             grassEaterArr.push(grEat)
-                        }else if(matrix[y][x] ==  3){
-                             let pre = new Predator(x,y)
-                             predatorArr.push(pre)
-                        }else if(matrix[y][x] == 4){
-                                let flow = new Flower(x,y)
-                                flowerArr.push(flow)
-                        }else if(matrix[y][x] == 5){
-                                let rab = new Rabbit(x,y)
-                                rabbitArr.push(rab)
-                        }else if(matrix[y][x] == 6){
-                                let wat = new Water(x,y)
-                                waterArr.push(wat)
-                        }else if(matrix[y][x] == 7){
-                                let fir = new Fire(x,y)
-                                fireArr.push(fir)
-                        }
-
-
-                }
-        }
+        createCanvas(25 * side, 25 * side)
+        
 
 }
 
 
-function draw() {
+function changeColor(matrix) {
         for (let y = 0; y < matrix.length; y++) {
                 for (let x = 0; x < matrix[y].length; x++) {
                         if (matrix[y][x] == 1) {
@@ -77,51 +40,9 @@ function draw() {
                 }
         }
 
+}
 
-
-                for (let i in grassArr) {
-                        grassArr[i].mul()
-                }
-
-
-                for(let i in grassEaterArr){
-                        grassEaterArr[i].eat()
-                }
-
-             
-
-                for(let i in predatorArr){
-                        predatorArr[i].eat()
-                }
- 
-
-                for(let i in flowerArr){
-                        flowerArr[i].eat()
-                }
-                for(let i in rabbitArr){
-                        rabbitArr[i].eat()
-                }
-                for(let i in rabbitArr){
-                        rabbitArr[i].die()
-                }
-                for(let i in waterArr){
-                        waterArr[i].mul()
-                }
-                for(let i in fireArr){
-                        fireArr[i].mul()
-                }
-                for(let i in waterArr){
-                        waterArr[i].eat()
-                }
-                for(let i in fireArr){
-                        fireArr[i].eat()
-                }
-                
-                
-                
- 
-
-        }
+socket.on('send matrix',changeColor)
 
 
 
--- server.js
+++ server.js
@@ -9,89 +9,184 @@ app.use(express.static("."));
 
 
 app.get('/', function (req, res) {
-   res.redirect('index.html');
+        res.redirect('index.html');
 });
 
-server.listen(3000,function(){
-    console.log("server is run")
+server.listen(3000, function () {
+        console.log("server is run")
 });
 
-function matrixGenerator(matrixSize, grass,grassEater,predator,flower,rabbit,water,fire) {
-    var matrix = []
-    ////  matrix սարքելու հատված
-    for (let i = 0; i < matrixSize; i++) {
-            matrix.push([])
-            for (let j = 0; j < matrixSize; j++) {
-                    matrix[i].push(0)
-            }
-    }
+function matrixGenerator(matrixSize, grass, grassEater, predator, flower, rabbit, water, fire) {
+        var matrix = []
+        ////  matrix սարքելու հատված
+        for (let i = 0; i < matrixSize; i++) {
+                matrix.push([])
+                for (let j = 0; j < matrixSize; j++) {
+                        matrix[i].push(0)
+                }
+        }
 
-    // 1 -եր այսինքն խոտեր քցելու հատված մատռիքսում
-    for (let i = 0; i < grass; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 1
-    }
-     //GrassEater 2
+        // 1 -եր այսինքն խոտեր քցելու հատված մատռիքսում
+        for (let i = 0; i < grass; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 1
+        }
+        //GrassEater 2
 
-     for (let i = 0; i < grassEater; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 2
-    }
-    //3 predator
-
-
-    for (let i = 0; i < predator; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 3
-    }
-
-    //4 flower
+        for (let i = 0; i < grassEater; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 2
+        }
+        //3 predator
 
 
-    for (let i = 0; i < flower; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 4
-    }
+        for (let i = 0; i < predator; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 3
+        }
 
-    //5 rabbit
-     
-
-    for (let i = 0; i < rabbit; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 5
-    }
-    //6 water
-
-    for (let i = 0; i < water; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 6
-    }
-    //7 fire
-
-    for (let i = 0; i < water; i++) {
-            let x = Math.floor(Math.random() * matrixSize)
-            let y = Math.floor(Math.random() * matrixSize)
-            matrix[y][x] = 7
-    }
+        //4 flower
 
 
-/////
+        for (let i = 0; i < flower; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 4
+        }
+
+        //5 rabbit
+
+
+        for (let i = 0; i < rabbit; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 5
+        }
+        //6 water
+
+        for (let i = 0; i < water; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 6
+        }
+        //7 fire
+
+        for (let i = 0; i < water; i++) {
+                let x = Math.floor(Math.random() * matrixSize)
+                let y = Math.floor(Math.random() * matrixSize)
+                matrix[y][x] = 7
+        }
+
+
+        /////
 
 
 
-    
 
-   
-   
-    return matrix
+
+
+
+        return matrix
 }
 
-matrix = matrixGenerator(25,17,5,10,50,5,1,1)
+matrix = matrixGenerator(25, 17, 5, 10, 50, 5, 1, 1)
 
-io.sockets.emit('send matrix',matrix)
+io.sockets.emit('send matrix', matrix)
+
+grassArr = []
+grassEaterArr = []
+predatorArr = []
+flowerArr = []
+rabbitArr = []
+waterArr = []
+fireArr = []
+
+Grass = require("./grass")
+GrassEater = require("./grassEater")
+Predator = require("./predator")
+Fire = require("./fire")
+Water = require("./water")
+Flower = require("./flower")
+Rabbit = require("./Rabbit")
+
+function createObject() {
+        for (let y = 0; y < matrix.length; y++) {
+                for (let x = 0; x < matrix[y].length; x++) {
+                        if (matrix[y][x] == 1) {
+                                let grass = new Grass(x, y)
+
+                                grassArr.push(grass)
+
+
+                        } else if (matrix[y][x] == 2) {
+                                let grEat = new GrassEater(x, y)
+                                grassEaterArr.push(grEat)
+                        } else if (matrix[y][x] == 3) {
+                                let pre = new Predator(x, y)
+                                predatorArr.push(pre)
+                        } else if (matrix[y][x] == 4) {
+                                let flow = new Flower(x, y)
+                                flowerArr.push(flow)
+                        } else if (matrix[y][x] == 5) {
+                                let rab = new Rabbit(x, y)
+                                rabbitArr.push(rab)
+                        } else if (matrix[y][x] == 6) {
+                                let wat = new Water(x, y)
+                                waterArr.push(wat)
+                        } else if (matrix[y][x] == 7) {
+                                let fir = new Fire(x, y)
+                                fireArr.push(fir)
+                        }
+
+
+                }
+        }
+        io.sockets.emit('send matrix', matrix)
+
+}
+
+
+function game() {
+        for (let i in grassArr) {
+                grassArr[i].mul()
+        }
+        for (let i in grassEaterArr) {
+                grassEaterArr[i].eat()
+        }
+        for (let i in predatorArr) {
+                predatorArr[i].eat()
+        }
+        for (let i in flowerArr) {
+                flowerArr[i].eat()
+        }
+        for (let i in rabbitArr) {
+                rabbitArr[i].eat()
+        }
+        for (let i in rabbitArr) {
+                rabbitArr[i].die()
+        }
+        for (let i in waterArr) {
+                waterArr[i].mul()
+        }
+        for (let i in fireArr) {
+                fireArr[i].mul()
+        }
+        for (let i in waterArr) {
+                waterArr[i].eat()
+        }
+        for (let i in fireArr) {
+                fireArr[i].eat()
+        }
+        io.sockets.emit('send matrix', matrix)
+
+}
+
+
+setInterval(game,300)
+
+io.on("connection",function(){
+        createObject()
+})
